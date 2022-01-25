367696885025d389ed8655362eb9c8d3ebaa7733
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-01-25 10:15:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,27 @@ secret = env.get("SECRET")
 token = env.get("TOKEN")
 host = env.get("HOST")
 invite = "https://discord.com/api/oauth2/authorize?client_id=915274079274164234&permissions=545394785527&scope=bot"
+supp = "https://discord.gg/UhThVBWWqA"
+support = 900078273021751296
+postgres_database_url = env.get("psql")
+
+
+from tortoise.backends.base.config_generator import expand_db_url
+
+
+tortoise = {
+  "connections": {
+    "default": expand_db_url(postgres_database_url),
+  },
+  "apps": {
+    "default": {
+      "models": [
+        "models"
+      ]
+    }
+  }
+}
+
+del expand_db_url
+
+tortoise["connections"]["default"]["credentials"]["ssl"] = "disable"
